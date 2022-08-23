24e536b8eaa5687ccee1d9e3c12624aa9b10ac9d
roma_brb <romeo22121993@gmail.com>
2022-08-23 17:24:50 +0300

    push

--- plugins/dkng-plugin/src/templates/single-svn_tpl.php
+++
@@ -1,4 +0,0 @@
-<?php
-	wp_redirect(get_site_url() );
-	exit;
-
--- themes/dkng/404.php
+++ themes/dkng/404.php
@@ -6,7 +6,7 @@ get_header('custom'); ?>
 
     <div id="content" class="row align-items-center justify-content-center page404" style="height: 100%;">
         <div class="container text-center">
-            <h2 class="text-white">
+            <h2 >
                 Такої сторінки не знайдено.
                 <br>Ідіть назад  до <a href="<?php echo home_url();?>"> Головної сторінки</a> і знайдіть ту сторінку
                 що ви шукали.
